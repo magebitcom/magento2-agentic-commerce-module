171464b2a5baa0815d7a938fe7faa000fcb083db
KristofersOzolinsMagebit <kristofers.ozolins@magebit.com>
2025-09-30 18:44:49 +0300

    init

---
+++ registration.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * @copyright Copyright (c) 2025 Magebit, Ltd. (https://magebit.com/)
+ * @author    Magebit <info@magebit.com>
+ * @license   GNU General Public License ("GPL") v3.0
+ */
+
+declare(strict_types=1);
+
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(ComponentRegistrar::MODULE, 'Magebit_AgenticCommerce', __DIR__);
