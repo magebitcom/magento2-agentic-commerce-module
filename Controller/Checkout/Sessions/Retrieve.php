c2667da40f1b4c4cfa3b483f59ee94d70d5b3b6d
KristofersOzolinsMagebit <kristofers.ozolins@magebit.com>
2025-10-02 23:40:16 +0300

    feat: fetch session

---
+++ Controller/Checkout/Sessions/Retrieve.php
@@ -0,0 +1,78 @@
+<?php
+/**
+ * This file is part of the Magebit_AgenticCommerce package.
+ *
+ * @copyright Copyright (c) 2025 Magebit, Ltd. (https://magebit.com/)
+ * @author    Magebit <info@magebit.com>
+ * @license   GNU General Public License ("GPL") v3.0
+ */
+
+declare(strict_types=1);
+
+namespace Magebit\AgenticCommerce\Controller\Checkout\Sessions;
+
+use Magebit\AgenticCommerce\Api\Data\Response\ErrorResponseInterface;
+use Magebit\AgenticCommerce\Api\Data\Response\ErrorResponseInterfaceFactory;
+use Magebit\AgenticCommerce\Controller\ApiController;
+use Magento\Framework\App\Action\HttpGetActionInterface;
+use Magento\Framework\App\RequestInterface;
+use Magento\Framework\App\Request\Http;
+use Magento\Framework\Controller\Result\JsonFactory;
+use Magento\Framework\Controller\ResultInterface;
+use Magebit\AgenticCommerce\Service\CheckoutSessionService;
+use Psr\Log\LoggerInterface;
+use Magebit\AgenticCommerce\Service\ComplianceService;
+use Magebit\AgenticCommerce\Model\Data\Response\CheckoutSessionResponse;
+
+class Retrieve extends ApiController implements HttpGetActionInterface
+{
+    public function __construct(
+        JsonFactory $resultJsonFactory,
+        RequestInterface $request,
+        protected readonly ComplianceService $complianceService,
+        protected readonly ErrorResponseInterfaceFactory $errorResponseFactory,
+        protected readonly LoggerInterface $logger,
+        protected readonly CheckoutSessionService $checkoutSessionService
+    ) {
+        parent::__construct($resultJsonFactory, $request);
+    }
+
+    /**
+     * Execute view action
+     *
+     * @return ResultInterface
+     */
+    public function execute(): ResultInterface
+    {
+        /** @var Http $request */
+        $request = $this->getRequest();
+
+        if ($validationError = $this->complianceService->validateRequest($request)) {
+            return $this->makeErrorResponse($validationError);
+        }
+
+        if ($response = $this->complianceService->handleIdempotency($request)) {
+            $this->addHeaders($response, $request);
+            return $response;
+        }
+
+        $sessionId = $request->getParam('session_id');
+
+        if (!$sessionId) {
+            $this->logger->critical('Session ID is required');
+
+            return $this->makeErrorResponse($this->errorResponseFactory->create([ 'data' => [
+                'type' => ErrorResponseInterface::TYPE_INVALID_REQUEST,
+                'code' => 'invalid_request',
+                'message' => 'Invalid request',
+            ]]));
+        }
+        /** @var string $sessionId */
+        /** @var CheckoutSessionResponse $response */
+        $response = $this->checkoutSessionService->retrieve((string) $sessionId);
+        $response = $this->makeJsonResponse($response);
+
+        $this->addHeaders($response, $request);
+        return $response;
+    }
+}
