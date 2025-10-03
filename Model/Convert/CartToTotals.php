373430aeab924a3e641626794e436ed1611d6e03
KristofersOzolinsMagebit <kristofers.ozolins@magebit.com>
2025-10-03 15:20:07 +0300

    chore: updated license

--- Model/Convert/CartToTotals.php
+++ Model/Convert/CartToTotals.php
@@ -1,5 +1,13 @@
 <?php
 
+/**
+ * This file is part of the Magebit_AgenticCommerce package.
+ *
+ * @copyright Copyright (c) 2025 Magebit, Ltd. (https://magebit.com/)
+ * @author    Magebit <info@magebit.com>
+ * @license   MIT
+ */
+
 namespace Magebit\AgenticCommerce\Model\Convert;
 
 use Magebit\AgenticCommerce\Api\Data\TotalInterface;
